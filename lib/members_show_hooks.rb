84d82d61c35845633aaea51866b936c7f0510274
cforce <cforce@gmx.de>
2012-07-04 14:18:00 +0200

    changed layout added routes.rb for redmine 1.4 compatibility added locale fixed redmine links for redmine 1.4

--- lib/members_show_hooks.rb
+++ lib/members_show_hooks.rb
@@ -1,5 +1,5 @@
 class MembersShowHooks  < Redmine::Hook::ViewListener
-  def view_projects_show_left(context = { })
+  def view_projects_show_right(context = { })
     project = context[:project]
     block = OverviewBlock.find_by_project_id_and_name(project,'members')
     context[:controller].send(:render_to_string, {:locals => context, :file => "/projects/members"}) if block
--- lib/news_show_hooks.rb
+++ lib/news_show_hooks.rb
@@ -6,7 +6,7 @@ class NewsShowHooks < Redmine::Hook::ViewListener
     context[:controller].send(:render_to_string, {:locals => context, :file => "projects/news"}) if block
   end
 
-  def view_welcome_index_left(context = { })
+  def view_welcome_index_right(context = { })
     block = HomeBlock.find_by_name('news')
     context[:controller].send(:render_to_string, {:locals => context, :file => "welcome/news"}) if block
   end
