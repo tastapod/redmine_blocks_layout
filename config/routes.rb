70a38a26a7d1962fc8fda43de3de6446613b6b6d
cforce <cforce@gmx.de>
2012-07-16 11:04:10 +0200

    fixed routes from time_entries to time_log

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'blocks/:action', :controller => 'blocks'
+  map.connect 'time_entries',:controller => 'time_log'
+  map.connect 'time_entries/:action',:controller => 'time_log'
+  map.connect 'time_entries/:action?project_id=:id',:controller => 'time_log'  
 end
