c2519f36901abad69351bfd9897c6f78c0bf1dd6
Ben Evans <jedifreeman@gmail.com>
2014-03-03 13:45:26 -0800

    check if polls/latest_forum_messages is installed before including in available modules

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -1,9 +1,15 @@
 module BlocksHelper
   def available_overview_blocks
-    ['members', 'news', 'polls', 'forum_messages', 'issues']
+    blocks = ['members','news','issues']
+    blocks.push('polls') if Redmine::Plugin.installed?(:redmine_polls)
+    blocks.push('forum_messages') if Redmine::Plugin.installed?(:redmine_last_messages)
+    return blocks
   end
   def available_home_blocks
-    ['news', 'polls', 'forum_messages','projects']
+    blocks = ['news', 'projects']
+    blocks.push('polls') if Redmine::Plugin.installed?(:redmine_polls)
+    blocks.push('forum_messages') if Redmine::Plugin.installed?(:redmine_last_messages)
+    return blocks
   end
 #  cattr_reader :available_overview_blocks
 end
