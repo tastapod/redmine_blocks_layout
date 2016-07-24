7b9a2bc2d61ab801176f34caf750524170c315aa
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-07-24 23:37:23 +0300

    match method w/o http method fixed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 if Rails::VERSION::MAJOR >= 3
-  match 'blocks/:action', :controller => 'blocks'
-  match 'time_entries', :controller => 'timelog'
-  match 'time_entries/:action',:controller => 'timelog'
-  match 'time_entries/:action?project_id=:id',:controller => 'timelog'
+  match 'blocks/:action', :controller => 'blocks', via: :all
+  match 'time_entries', :controller => 'timelog', via: :all
+  match 'time_entries/:action',:controller => 'timelog', via: :all
+  match 'time_entries/:action?project_id=:id',:controller => 'timelog', via: :all
 else
   ActionController::Routing::Routes.draw do |map|
     map.connect 'blocks/:action', :controller => 'blocks'
